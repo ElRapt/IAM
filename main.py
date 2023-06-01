4860d18bdd42df1840d91dd5be416bcb79cedde0
Lucas Denis <lucas.denis@etu.uca.fr>
2023-06-01 14:49:37 +0200

    Some changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import tweepy
 import time
 
+# Loading JSONS
 credentials = json.load(open("credentials.json"))
 dictionary = json.load(open("dictionary.json"))
 
@@ -18,19 +19,13 @@ bearer_token = credentials["bearer_token"]
 # Creating client with necessary tokens
 client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
 
-# Creating tweet
-#response = client.create_tweet(text="Hello, Twitter!")
-
-# Printing response
-#print(response)
-
 while counter < len(dictionary):
     message = "I am " + dictionary[counter]
     try:
         client.create_tweet(text=message)
     except tweepy.TweepError as e:
         print("Erreur lors de l'envoi du tweet :", e)
-        time.sleep(60)  # Attend 1 minute avant de réessayer
+        time.sleep(60)  # Wait 1 minute before trying again
 
     counter +=1
-    time.sleep(10)  # Attend 30 minutes avant d'envoyer le prochain tweet
+    time.sleep(30*60)  # Wait 30 minutes before sending the next tweet
