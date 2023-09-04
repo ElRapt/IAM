db3ce94b0ed181e24081a24632429083acbebbfc
Lucas Denis <lucas.denis@etu.uca.fr>
2023-09-04 21:03:56 +0200

    Large improvements

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import json
+import tweepy
 import asyncio
 import logging
-from typing import Any
-from tweepy import Client
+import threading
 
 logging.basicConfig(level=logging.INFO)
-
-async def tweet_word(client: Client, dictionary: list[str], counter: int) -> None:
-    """Tweet a word from the dictionary."""
-    try:
-        word = dictionary[counter]
-        logging.info(f"Tweeting the word: {word}")
-        # Assume tweet() is an async method
-        await client.tweet(word)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
 
 async def main():
     credentials = json.load(open("credentials.json"))
     dictionary = json.load(open("dictionary.json"))
-    counter = 0
 
-    client = Client(
-        bearer_token=credentials["bearer_token"],
-        consumer_key=credentials["api_key"],
-        consumer_secret=credentials["api_key_secret"],
-        access_token=credentials["access_token"],
-        access_token_secret=credentials["access_token_secret"],
+    counter = 0
+    counter_lock = threading.Lock()
+
+    consumer_key = credentials["api_key"]
+    consumer_secret = credentials["api_key_secret"]
+    access_token = credentials["access_token"]
+    access_token_secret = credentials["access_token_secret"]
+    bearer_token = credentials["bearer_token"]
+
+    client = tweepy.Client(
+        bearer_token=bearer_token,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
     )
 
-    while True:
-        await tweet_word(client, dictionary, counter)
-        counter = (counter + 1) % len(dictionary)
-        await asyncio.sleep(60)  # Sleep for 60 seconds
+    while counter < len(dictionary):
+        message = "I am " + dictionary[counter]
+
+        try:
+            client.create_tweet(text=message)
+            logging.info(f"Successfully tweeted: {message}")
+        except tweepy.TweepError as e:
+            logging.error(f"Error while sending tweet: {e}")
+            await asyncio.sleep(60)  # Wait 1 minute before trying again
+
+        with counter_lock:
+            counter += 1
+
+        await asyncio.sleep(30 * 60)  # Wait 30 minutes before sending the next tweet
 
 if __name__ == '__main__':
     asyncio.run(main())
